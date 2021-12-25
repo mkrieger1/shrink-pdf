69d110e3c490f71648feba52a5f55195b3776a07
mkrieger1 <mkrieger1@users.noreply.github.com>
2021-12-25 20:08:28 +0100

    Write output file in Python, not by GhostScript
    Using `-dSAFER` apparently doesn't prevent overwriting a file with
    `-sOutputFile` but it can't hurt to use it nevertheless.

--- shrink_pdf.py
+++ shrink_pdf.py
@@ -3,11 +3,18 @@ import sys
 from os.path import basename, splitext
 
 
-def call_gs(infile: str, outfile: str):
-    subprocess.check_call([
-        'gs', '-sDEVICE=pdfwrite', '-dPDFSETTINGS=/ebook',
-        '-dNOPAUSE', '-dBATCH', f'-sOutputFile={outfile}', infile
+def call_gs(infile: str) -> bytes:
+    return subprocess.check_output([
+        'gs', '-dNOPAUSE', '-dBATCH',
+        '-dSAFER', '-q', '-sOutputFile=-',
+        '-sDEVICE=pdfwrite', '-dPDFSETTINGS=/ebook',
+        infile
     ])
+
+
+def write_result(result: bytes, outfile: str) -> None:
+    with open(outfile, 'wb') as f:
+        f.write(result)
 
 
 def main() -> None:
@@ -26,6 +33,7 @@ def main() -> None:
             f"Usage: {basename(sys.argv[0])} input.pdf [output.pdf]")
 
     try:
-        call_gs(infile, outfile)
+        result = call_gs(infile)
     except subprocess.CalledProcessError as e:
         raise SystemExit(e)
+    write_result(result, outfile)
